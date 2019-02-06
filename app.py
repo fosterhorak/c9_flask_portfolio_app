09443aa9eaa437e76346da723ea6a2187c1c9851
fosterhorak <47340502+fosterhorak@users.noreply.github.com>
2019-02-05 20:20:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	new name = str(name) + " (you know you want to)"
+	return render_template('index.html', name=new name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
